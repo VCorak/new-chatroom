0423d2756430bc2229e2ff7fff420e24461586d9
Valentina Corak <valentinacorak@gmail.com>
2021-08-16 16:57:14 +0200

    Capture message and emit with broadcast event

--- server/server.js
+++ server/server.js
@@ -69,3 +69,41 @@ io.on('connection', (socket) => {
     console.log('👾 New socket connected! >>', socket.id);
 });
 
+// need to capture the new-message event using the socket.on() method and emit the broadcast-message event
+
+// To save the list of users as id:username
+let users = {}
+
+io.on('connection', (socket) => {
+    console.log('👾 New socket connected! >>', socket.id)
+
+    // handles new connection
+    socket.on('new-connection', (data) => {
+        // captures event when new clients join
+        console.log(`new-connection event received`, data)
+        // adds user to list
+        users[socket.id] = data.username
+        console.log('users :>> ', users)
+        // emit welcome message event
+        socket.emit('welcome-message', {
+            user: 'server',
+            message: `Welcome to this chat ${data.username}. There are ${
+                Object.keys(users).length
+            } users connected`,
+        })
+    })
+// handles message posted by client
+    socket.on('new-message', (data) => {
+        console.log(`👾 new-message from ${data.user}`)
+        // broadcast message to all sockets except the one that triggered the event
+        socket.broadcast.emit('broadcast-message', {
+            user: users[data.user],
+            message: data.message,
+        })
+    })
+})
+
+// when the server receives the new-message event,
+// now we're using socket.broadcast.emit() instead of
+// just socket.emit().
+// The difference is that when broadcasting, we're sending an event to all clients except the one that sent the original event
